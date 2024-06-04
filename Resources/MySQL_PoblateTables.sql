2f279cf49907f9664234aba02b97b652b2c7b870
Diego <gimbletdev@gmail.com>
2024-06-03 21:09:22 -0500

    Avance en Procedimientos Almacenados
    Se crearon una multitud de procedimientos almacenados, unos mas complejos que otros, pero aun quedan

--- Resources/MySQL_PoblateTables.sql
+++ Resources/MySQL_PoblateTables.sql
@@ -140,6 +140,52 @@ BEGIN
 END$$
 DELIMITER ;
 
+-- VERIFICA QUE EL TELEFONO DE UN USUARIO SEAN VALIDOS, RETORNA 0 SI ES INVALIDO, UNO SI AMBOS SON VALIDOS
+
+DELIMITER $$
+	CREATE FUNCTION FN_VerificarNumeroyCodigo
+    (
+		Codigo INT,
+        Telefono CHAR(9)
+    )
+		RETURNS INT 
+		DETERMINISTIC
+	BEGIN
+		DECLARE Resultado INT;
+        
+		SELECT IF	(COD_USU = Codigo, 1, 0)
+        INTO 		Resultado
+		FROM 		LOGINS.USUARIO 
+		WHERE 		COD_USU = Codigo;
+        
+        SELECT IF	(TEL_USU = Telefono, 0, 1) 
+        INTO 		Resultado
+        FROM 		LOGINS.USUARIO
+        WHERE 		TEL_USU = Telefono;
+        
+        RETURN Resultado;
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+	CREATE FUNCTION FN_VerificarTipoUsuario
+    (
+		Codigo INT
+    )
+		RETURNS VARCHAR(15) 
+		DETERMINISTIC
+	BEGIN
+		DECLARE Resultado VARCHAR(15);
+        
+		SELECT TIP_USU INTO Resultado
+        FROM LOGINS.USUARIO
+        WHERE COD_USU = Codigo;
+        
+        RETURN Resultado;
+    END $$
+DELIMITER ;
+
+
 -- ---------------------------------  CREAR STORED PROCEDURES ----------------------------------
 -- REGISTRAR CLIENTE
 DELIMITER $$
@@ -217,7 +263,7 @@ BEGIN
 	INSERT INTO LOGINS.USUARIO
 		(DNI_USU, TIP_USU, NOM_USU, APE_USU, TEL_USU, CON_USU)
 	VALUES
-		(Dni, 'Admin', Nombres, Apellidos, Telefono, Clave);
+		(Dni, 'HeadAdmin', Nombres, Apellidos, Telefono, Clave);
         
 	SET CODIGO = FN_GetUltimoUsuario();
         
@@ -233,10 +279,239 @@ SELECT * FROM LOGINS.USUARIO u Inner Join Logins.usuario_cliente c on  u.cod_usu
 SELECT * FROM LOGINS.USUARIO u Inner Join Logins.usuario_admin a on  u.cod_usu = a.COD_ADM;
 SELECT * FROM LOGINS.USUARIO u Inner Join Logins.usuario_headadmin h on  u.cod_usu = h.COD_HEA;
 
+-- CONSULTAR/BUSCAR CLIENTES
+DELIMITER $$
+CREATE PROCEDURE SP_ListarClientes()
+BEGIN
+	SELECT 	U.COD_USU, U.DNI_USU, U.TIP_USU, U.NOM_USU, U.APE_USU, U.TEL_USU, U.CON_USU, U.FCC_USU,
+			C.SAL_CLI
+    FROM LOGINS.USUARIO 				AS U
+    INNER JOIN LOGINS.USUARIO_CLIENTE 	AS C
+    ON C.COD_CLI = U.COD_USU;
+END $$
+DELIMITER ;
+
+CALL SP_ListarClientes();
+
+-- CONSULTAR/BUSCAR ADMINS
+DELIMITER $$
+CREATE PROCEDURE SP_ListarAdmins()
+BEGIN
+	SELECT 	U.COD_USU, U.DNI_USU, U.TIP_USU, U.NOM_USU, U.APE_USU, U.TEL_USU, U.CON_USU, U.FCC_USU,
+			A.SUB_ADM, A.COM_ADM, A.CAC_ADM, A.SUM_ADM
+    FROM LOGINS.USUARIO 				AS U
+    INNER JOIN LOGINS.USUARIO_ADMIN 	AS A
+    ON U.COD_USU = A.COD_ADM;
+END $$
+DELIMITER ;
+
+CALL SP_ListarAdmins();
+
+-- CONSULTAR/BUSCAR HEADADMINS
+DELIMITER $$
+CREATE PROCEDURE SP_ListarHeadAdmins()
+BEGIN
+	SELECT 	U.COD_USU, U.DNI_USU, U.TIP_USU, U.NOM_USU, U.APE_USU, U.TEL_USU, U.CON_USU, U.FCC_USU,
+			H.SUE_HEA
+    FROM LOGINS.USUARIO 					AS U
+    INNER JOIN LOGINS.USUARIO_HEADADMIN 	AS H
+    ON U.COD_USU = H.COD_HEA;
+END $$
+DELIMITER ;
+
+CALL SP_ListarHeadAdmins();
+
+-- LISTAR DATOS DEL CLIENTE A RECIBIR UN YAPE
+DELIMITER $$
+CREATE PROCEDURE SP_BuscarClientexNumero
+(
+	Telefono CHAR(9)
+)
+BEGIN
+	SELECT 	U.NOM_USU, U.APE_USU, U.TEL_USU
+    FROM LOGINS.USUARIO 				AS U
+    INNER JOIN LOGINS.USUARIO_CLIENTE 	AS C
+    ON C.COD_CLI = U.COD_USU
+    WHERE U.TEL_USU = Telefono;
+END $$
+DELIMITER ;
+
+CALL SP_BuscarClientexNumero('945888777');
+
+-- ACTUALIZAR DATOS DE UN CLIENTE
+/*
+	El procedimiento valida que el telefono ingresado no exista y el codigo sea valido con la funcion VerificarNumero y Codigo
+	Si todo es correcto, La variable Valido sera = 1 , de lo contrario la variable sera 0. En caso la variable sea 0 se abandona el procedimiento
+    Se verificar que el codigo ingresado pertenezaca a un cliente, de lo contrario se imprime un mensaje y se abandona el proceimiento
+    Luego se guarda el telefono original del usuario en una variable OGTelefono
+    Se procede a hcaer un update de los campos ingresados
+    Se elimina la restriccion de los foreign key de la tabla Transacciones Yape para poder modificar el telefono del usuario
+    Se actualiza los campos donde concida el numero original con el numero anterior
+    Se vuelve a activar las foreign keys en la tabla yape
+    Se imprime un mensaje de confirmacion
+*/
+DELIMITER $$
+CREATE PROCEDURE SP_ActualizarCliente
+(
+	Codigo 		INT,
+    Dni  		CHAR(8),
+    Nombres 	VARCHAR(50),
+    Apellidos 	VARCHAR(50),
+    Telefono 	CHAR(9),
+    Clave 		VARCHAR(100),
+    Saldo		DOUBLE
+) Procedimiento:
+BEGIN
+	DECLARE Valido INT;
+    DECLARE OGTelefono CHAR(9);
+    DECLARE Tipo VARCHAR(15);
+    SET 	Valido = FN_VerificarNumeroyCodigo(Codigo, Telefono);
+    SET 	Tipo = FN_VerificarTipoUsuario(Codigo);
+
+	IF (Valido = 0) THEN
+		SELECT 'El Codigo no Existe o el Telefono ingresado ya se encuentra registrado' AS Mensaje;
+		LEAVE Procedimiento;
+	END IF;
+    
+    IF(Tipo != 'Cliente') THEN
+		SELECT 'El Codigo Ingresado No Pertenece a un Cliente' AS Mensaje;
+        LEAVE Procedimiento;
+	END IF;
+    
+	SELECT 	TEL_USU 
+    INTO 	OGTelefono 
+	FROM 	LOGINS.USUARIO 
+	WHERE 	COD_USU = Codigo;
+
+	UPDATE 	LOGINS.USUARIO_CLIENTE
+	SET 	SAL_CLI = Saldo
+	WHERE 	COD_CLI = Codigo;
+    
+    ALTER TABLE TRANSACCIONES.YAPE
+		DROP FOREIGN KEY yape_ibfk_1;
+	ALTER TABLE TRANSACCIONES.YAPE
+		DROP FOREIGN KEY yape_ibfk_2;
+
+	UPDATE LOGINS.USUARIO 
+	SET DNI_USU = Dni, 
+		NOM_USU = Nombres, 
+		APE_USU = Apellidos,
+		TEL_USU = Telefono,
+		CON_USU = Clave
+	WHERE COD_USU = Codigo;
+
+	UPDATE TRANSACCIONES.YAPE
+		SET NRC_YAP = Telefono
+	WHERE NRC_YAP = OGTelefono;
+
+	UPDATE TRANSACCIONES.YAPE
+		SET NRT_YAP = Telefono
+	WHERE NRT_YAP = OGTelefono;
+		
+	ALTER TABLE TRANSACCIONES.YAPE
+	ADD CONSTRAINT yape_ibfk_1
+	FOREIGN KEY (NRC_YAP) REFERENCES LOGINS.USUARIO(TEL_USU);
+
+	ALTER TABLE TRANSACCIONES.YAPE
+	ADD CONSTRAINT yape_ibfk_2
+	FOREIGN KEY (NRT_YAP) REFERENCES LOGINS.USUARIO(TEL_USU);
+    
+    SELECT 'Datos Actualizados Correctamente' AS Mensaje;
+END $$
+DELIMITER ;
+
+CALL SP_ActualizarCliente(2, '91893044', 'Miguel Ivan', 'Rodriguez Campos', '948930132', 'Miguel032', 1204.51);
+
+DELIMITER $$
+CREATE PROCEDURE SP_ActualizarAdminSP
+(
+	Codigo 		INT,
+    Dni  		CHAR(8),
+    Nombres 	VARCHAR(50),
+    Apellidos 	VARCHAR(50),
+    Telefono 	CHAR(9),
+    Clave 		VARCHAR(100)
+) Procedimiento:
+BEGIN
+	DECLARE Valido INT;
+    DECLARE OGTelefono CHAR(9);
+    DECLARE Tipo VARCHAR(15);
+    SET 	Valido = FN_VerificarNumeroyCodigo(Codigo, Telefono);
+    SET 	Tipo = FN_VerificarTipoUsuario(Codigo);
+
+	IF (Valido = 0) THEN
+		SELECT 'El Codigo no Existe o el Telefono ingresado ya se encuentra registrado' AS Mensaje;
+		LEAVE Procedimiento;
+    END IF;
+    
+    IF(Tipo != 'Admin') THEN
+		SELECT 'El Codigo Ingresado No Pertenece a un Administrador' AS Mensaje;
+        LEAVE Procedimiento;
+	END IF;
+    
+    UPDATE LOGINS.USUARIO
+    SET DNI_USU = Dni, 
+		NOM_USU = Nombres, 
+		APE_USU = Apellidos,
+		TEL_USU = Telefono,
+		CON_USU = Clave
+	WHERE COD_USU = Codigo;
+    
+    SELECT 'Datos Actualizados Correctamente' AS Mensaje;
+END $$
+DELIMITER ;
+
+CALL SP_ActualizarAdminSP(5, '93847322', 'Alexa Maria', 'Mendoza Flores', '903844224', 'Alexa48h');
+
+DELIMITER $$
+CREATE PROCEDURE SP_ActualizarHeadAdmin
+(
+	Codigo 		INT,
+    Dni  		CHAR(8),
+    Nombres 	VARCHAR(50),
+    Apellidos 	VARCHAR(50),
+    Telefono 	CHAR(9),
+    Clave 		VARCHAR(100),
+    Sueldo 		DOUBLE
+) Procedimiento:
+BEGIN
+	DECLARE Valido INT;
+    DECLARE OGTelefono CHAR(9);
+    DECLARE Tipo VARCHAR(15);
+    SET 	Valido = FN_VerificarNumeroyCodigo(Codigo, Telefono);
+    SET 	Tipo = FN_VerificarTipoUsuario(Codigo);
+
+	IF (Valido = 0) THEN
+		SELECT 'El Codigo no Existe o el Telefono ingresado ya se encuentra registrado' AS Mensaje;
+		LEAVE Procedimiento;
+	END IF;
+    
+    IF(Tipo != 'HeadAdmin') THEN
+		SELECT 'El Codigo Ingresado No Pertenece a un Administrador Superior (HeadAdmin)' AS Mensaje;
+        LEAVE Procedimiento;
+	END IF;
+    
+    UPDATE LOGINS.USUARIO_HEADADMIN
+    SET SUE_HEA = Sueldo
+    WHERE COD_HEA = Codigo;
+    
+    UPDATE LOGINS.USUARIO
+    SET DNI_USU = Dni, 
+		NOM_USU = Nombres, 
+		APE_USU = Apellidos,
+		TEL_USU = Telefono,
+		CON_USU = Clave
+	WHERE COD_USU = Codigo;
+    
+    SELECT 'Datos Actualizados Correctamente' AS Mensaje;
+END $$
+DELIMITER ;
+
+CALL SP_ActualizarHeadAdmin(6, '83748233', 'Isaias Raul', 'Pascal Ramos', '923749400', 'Isaias38kA', 4313.51);
+    
 -- ----- TO DO
 
--- Crear procedimientos para listar los datos de los usuarios
--- Crear Procedimientos para actualizar los campos de los usuarios
+-- Crear Procedimiento para actualizar la comision, sueldo y sueldo del mes de un administrador (tener en cuenta que la suma de las comisiones y el sueldo base da el sueldo final)
 -- Crear Procedimientos para eliminar los campos usuarios (Crear tabla para almacenar Yapes/ agregar campo a clientes para habilitar/deshabilitar cuenta)
 
 -- Crear Procedimientos para actualizar los campos de un yape
